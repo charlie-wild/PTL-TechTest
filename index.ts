db2eb918181670241d890efcb56c5bd30d752f18
Charli Wild <charlie.wild@thinkmoney.co.uk>
2021-12-01 19:00:40 +0000

    added types for variables

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
-const getCurrentStockLevel = (sku) => {
+interface Transaction {
+    sku: string;
+    qty: number;
+    type: string;
+}
+
+interface StockItem {
+    sku: string;
+    stock: number;
+}
+
+
+const getCurrentStockLevel = (sku: string) => {
     return new Promise((resolve, reject) => {
-        const stock = require('./data/stock.json');
-        const transactions = require('./data/transactions.json');
+        const stock: Array<StockItem> = require('./data/stock.json');
+        const transactions: Array<Transaction> = require('./data/transactions.json');
 
         const existsInStock = stock.filter(item => item.sku === sku).length > 0;
         const existsInTransactions = transactions.filter(item => item.sku === sku).length > 0;
@@ -28,17 +40,19 @@ const getCurrentStockLevel = (sku) => {
 }
 
 
-const calcualateStockLevelChange = (transactionsMade) => {
+const calcualateStockLevelChange = (transactionsMade: Array<Transaction>) => {
     let totalStock = 0;
-    for (let i = 0; i < transactionsForSku.length; i++) {
-        if (transactionsForSku[i].type === 'order') {
-            totalStock -= transactionsForSku[i].qty;
+    for (let i = 0; i < transactionsMade.length; i++) {
+        if (transactionsMade[i].type === 'order') {
+            totalStock -= transactionsMade[i].qty;
         } else {
-            totalStock += transactionsForSku[i].qty;
+            totalStock += transactionsMade[i].qty;
         }
     }
     return totalStock;
 }
+
+
 
 
 
