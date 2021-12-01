e98465083106b8e44e31b7e25a02cdaacc1f7427
Charli Wild <charlie.wild@thinkmoney.co.uk>
2021-12-01 19:40:51 +0000

    added more type checking

--- index.ts
+++ index.ts
@@ -15,24 +15,24 @@ const getCurrentStockLevel = (sku: string): Promise<{ sku: string, qty: number }
         const stock: Array<StockItem> = require('./data/stock.json');
         const transactions: Array<Transaction> = require('./data/transactions.json');
 
-        const existsInStock = stock.filter(item => item.sku === sku).length > 0;
-        const existsInTransactions = transactions.filter(item => item.sku === sku).length > 0;
+        const existsInStock: boolean = stock.filter(item => item.sku === sku).length > 0;
+        const existsInTransactions: boolean = transactions.filter(item => item.sku === sku).length > 0;
 
         if (!existsInStock && !existsInTransactions) {
             reject(new Error(`SKU ${sku} does not exist in stock.json or transactions.json`));
         }
 
-        let initialStockLevel = 0;
+        let initialStockLevel: number = 0;
 
         if (existsInStock) {
             initialStockLevel = stock.find(item => item.sku === sku).stock;
         }
 
-        const transactionsMade = transactions.filter(transaction => transaction.sku === sku);
+        const transactionsMade: Transaction[] = transactions.filter(transaction => transaction.sku === sku);
 
-        const totalStockChange = calcualateStockLevelChange(transactionsMade);
+        const totalStockChange: number = calcualateStockLevelChange(transactionsMade);
 
-        const currentStockLevel = initialStockLevel + totalStockChange;
+        const currentStockLevel: number = initialStockLevel + totalStockChange;
 
         resolve({ sku, qty: currentStockLevel });
 
